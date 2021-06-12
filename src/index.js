65303b3a43b70635d6ebd4112490e02fd1a16230
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-06-12 15:40:15 -0500

    provide store for redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./index.css"
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
