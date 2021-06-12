7c8bc8a0aba8fba928b2bc4ebe97dee377cd5cc3
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-06-12 15:39:36 -0500

    add user auth calls and break into home page and groups components

--- src/App.js
+++ src/App.js
@@ -1,71 +1,36 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import './index.css' // tailwind
+import GroupPage from './components/GroupPage'
+import Register from "./components/Register";
+import Login from "./components/Login";
+import GuardedRoute from './components/GuardedRoute'
+import HomePage from './components/HomePage'
 import { useEffect, useState } from "react";
-import { ReactTinyLink } from 'react-tiny-link'
 
 function App() {
-    const [groups, setGroups] = useState([])
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const courses = []
-        const form = e.target
-        const groupName = form[0].value
-
-        for (let i = 1; i < form.length - 1; i++) {
-            courses.push({url: form[i].value})
-        }
-
-        const groupFormData = {groupName: groupName, courses: courses}
-
-        let res = await fetch("/creategroup", {
-            method:"POST",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(groupFormData)
-        })
-    }
+    const [isLoggedIn, setIsLoggedIn] = useState("")
 
     useEffect(() => {
-        fetch("/courses")
-        .then(res => res.json())
-        .then(data => {
-            setGroups(data)
+        fetch("/isUserAuth", {
+            headers: {
+                "x-access-token": localStorage.getItem("token")
+            }
         })
-        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(data => setIsLoggedIn(data.isLoggedIn))
     }, [])
 
     return (
-        <div className="">
-            <form className="m-10 text-md font-extrabold" onSubmit={(e) => handleSubmit(e)}>
-                <label htmlFor="group-name">First Name</label>
-                <input id="group-name" name="group-name" type="text" />
-                <label htmlFor="course-one">URL of Course 1</label>
-                <input id="course-two" name="course-one" type="url" />
-                <label htmlFor="course-one">URL of Course 2</label>
-                <input id="course-two" name="course-two" type="url" />
-                <input type="submit" value="Submit" />
-            </form>
-
-            {groups.map((group) => {
-                return (
-                    <div>
-                        <h1>{group.groupName}</h1>
-                        {group.courses.map(course => {
-                            // do open graph stuff here
-                            return (
-                                <ReactTinyLink
-                                    cardSize="small"
-                                    showGraphic={true}
-                                    maxLine={2}
-                                    minLine={1}
-                                    url={course.url || "https://en.wikipedia.org/wiki/HTTP_404"}
-                                />
-                            )
-                        })}
-                    </div>
-                )
-            })}
-
+        <div>
+            <BrowserRouter>
+                <Route component={Register} exact path="/register"/>
+                <Route component={Login}  exact path="/login"/>
+                <GuardedRoute component={GroupPage} isLoggedIn={isLoggedIn} exact path="/g/:groupId" />
+                
+                {/* <Route exact path="/" component={HomePage}/> */}
+                <GuardedRoute component={HomePage} isLoggedIn={isLoggedIn} exact path="/"/>
+            </BrowserRouter>
         </div>
     );
 }
