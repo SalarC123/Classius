1e3bc81e328d4787a47c84930587f17ccbea85bf
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-06-23 13:03:30 -0500

    init landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,18 @@
+function LandingPage() {
+
+    return (
+        <div>
+            <section className="h-screen bg-green-400">
+                Landing Page Section 1
+            </section>
+            <section className="h-screen bg-red-400">
+                Landing Page Section 2
+            </section>
+            <section className="h-screen bg-blue-400">
+                Landing Page Section 3
+            </section>
+        </div>
+    )
+}   
+
+export default LandingPage
