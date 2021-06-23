7a6151bf449c76e9b10b62147ae1ac55007197a3
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-06-23 13:02:33 -0500

    conditionally render login/logout/register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useLayoutEffect, useState } from 'react'
 function Navbar() {
 
     const history = useHistory()
-    const [username, setUsername] = useState("")
+    const [username, setUsername] = useState(null)
 
     async function logout() {
         localStorage.removeItem("token")
@@ -18,16 +18,26 @@ function Navbar() {
             }
         })
         .then(res => res.json())
-        .then(data => data.isLoggedIn ? setUsername(data.username): history.push("/login"))
+        .then(data => data.isLoggedIn ? setUsername(data.username): null)
     }, [])
 
     return (
-        <div className="flex flex-row p-10 justify-center text-2xl text-white">
-            <div className="mx-10 hover:text-green-300"><Link to="/">Home</Link></div>
-            <div className="mx-10 hover:text-green-300"><Link to="/login">Login</Link></div>
-            <div className="mx-10 hover:text-green-300"><Link to="/register">Register</Link></div>
-            <div className="cursor-pointer mx-10 hover:text-green-300" onClick={logout}>Logout</div>
-            <Link to={"/u/" + username} className="cursor-pointer mx-10 hover:text-green-300">Profile</Link>
+        <div className="flex flex-row p-10 justify-end text-2xl text-white">
+            <a href="/dashboard" className="mr-auto font-extrabold text-2xl">Classius</a>
+            <div className="hidden sm:flex mx-3 hover:text-green-300"><Link to="/dashboard">Home</Link></div>
+            {username  
+                ? <div className="hidden sm:flex">
+                    <Link to={"/u/" + username} className="cursor-pointer mx-3 hover:text-green-300">Profile</Link>
+                    <div className="cursor-pointer mx-3 hover:text-green-300" onClick={logout}>Logout</div>
+                  </div>  
+                : <div className="hideen sm:flex">
+                    <div className="mx-3 px-2 py-1 rounded font-bold text-xl bg-green-400 text-gray-900"><Link to="/login">Login</Link></div>
+                    <div className="mx-3 px-2 py-1 rounded font-bold text-xl border-2 border-green-400 text-green-400"><Link to="/register">Register</Link></div>
+                  </div>
+            }
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 flex sm:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
         </div>
     )
 }
