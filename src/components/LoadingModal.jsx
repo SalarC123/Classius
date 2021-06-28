c10e2dee60c4b6f2ad08cb834d7682f290064122
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-06-28 11:36:55 -0500

    add style changes

--- src/components/LoadingModal.jsx
+++ src/components/LoadingModal.jsx
@@ -4,7 +4,7 @@ function LoadingModal({isOpen}) {
 
     return (
         <div className={`${isOpen ? "flex": "hidden"} flex-col border-8 border-green-400 justify-center items-center fixed top-1/2 left-1/2 -mx-32 -my-32 w-64 h-64 rounded bg-white`}>
-            <img src={bars} alt="" />
+            <img src={bars} alt="Loading Bars" />
             <h1 className="text-2xl m-4 font-extrabold text-green-400">Creating Group...</h1>
         </div>
     )
