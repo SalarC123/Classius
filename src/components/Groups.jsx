24d7e512bcc93603a5f7fa744bc31eefbab2ef18
SalarC123 <salarwcheema@gmail.com>
2021-06-28 11:39:15 -0500

    add accessibility alt for img tag

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -35,11 +35,11 @@ function Groups() {
                                 <div className="grid grid-cols-2 grid-rows-2 gap-2">
                                     {/* Only display images of the first four courses */}
                                     {getFirstFourCourses(group).map(course => (
-                                        <img className="w-full" src={course.ogImage} onError={(e)=>{e.target.onerror = null; e.target.src=""}}/>
+                                        <img className="w-full" src={course.ogImage} alt="Custom Course" onError={(e)=>{e.target.onerror = null; e.target.src=""}}/>
                                     ))}
                                 </div>
                                 <Link to={"/g/" + group.routeId} className="text-green-400 inline-flex items-center mt-3">See Courses
-                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path d="M5 12h14"></path>
                                     <path d="M12 5l7 7-7 7"></path>
                                     </svg>
