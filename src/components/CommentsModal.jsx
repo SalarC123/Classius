a9536bd3946de2596d8b9c0312b3e0286e617722
SalarC123 <salarwcheema@gmail.com>
2021-07-01 13:44:52 -0500

    make custom validation error text and add list key

--- src/components/CommentsModal.jsx
+++ src/components/CommentsModal.jsx
@@ -2,7 +2,6 @@ import { useLayoutEffect } from "react";
 import { useState } from "react";
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import ValidationError from "./ValidationError";
 
 function CommentsModal({courseIdx, groupName}) {
 
@@ -54,43 +53,46 @@ function CommentsModal({courseIdx, groupName}) {
     }
 
     return (
-        <div className={`px-4 ${isCommentsModalOpen ? "flex": "hidden"} items-center flex-col fixed left-1/2 top-1/2 w-72 h-96 -mx-36 -my-48 sm:w-96 sm:h-192 sm:-mx-52 sm:-my-32 md:-mx-96 md:-my-48 md:w-192 md:h-auto bg-white text-black`}>
-            <button className="ml-auto pt-4" onClick={closeModal}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-            <h1 className="text-3xl font-extrabold">Comments</h1>
+        <div className="flex justify-center items-center">
+            <div className={`${isCommentsModalOpen ? "flex": "hidden"} bg-gray-900 z-40 opacity-90 inset-0 fixed`}></div>
+            <div className={`px-4 ${isCommentsModalOpen ? "flex": "hidden"} items-center top-3 sm:top-5 lg:top-72 flex-col fixed w-11/12 h-5/6 lg:h-192 z-50  md:w-192 md:h-auto bg-white text-black`}>
+                <button className="ml-auto pt-4" onClick={closeModal}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+                <h1 className="text-3xl font-extrabold">Comments</h1>
 
-            {course
-                ? <h1>{course.ogTitle}</h1>
-                : <div>Select a course...</div>
-            }
-
-            <div className="overflow-y-scroll break-all h-96 mr-auto">
                 {course
-                    ? course.comments.map(comment => (
-                        <div className="bg-gray-200 border-black mx-2 my-4 flex flex-row items-center">
-                            <img className="w-10 h-10 mx-2" src={comment.authorPfp} alt="" />
-                            <div className="flex flex-col p-2">
-                                <Link to={"/u/" + comment.author} className="font-bold hover:underline hover:text-blue-500">{comment.author}</Link>
-                                <p>{comment.text}</p>
-                            </div>
-                        </div>
-                    ))
-                    : <div></div>
+                    ? <h1>{course.ogTitle}</h1>
+                    : <div>Select a course...</div>
                 }
+
+                <div className="overflow-y-scroll break-all h-96 mr-auto ml-auto">
+                    {course
+                        ? course.comments.map(comment => (
+                            <div key={comment.id} className="bg-gray-200 border-black mx-2 my-4 flex flex-row items-center">
+                                <img className="w-10 h-10 mx-2" src={comment.authorPfp} alt="" />
+                                <div className="flex flex-col p-2">
+                                    <Link to={"/u/" + comment.author} className="font-bold hover:underline hover:text-blue-500">{comment.author}</Link>
+                                    <p>{comment.text}</p>
+                                </div>
+                            </div>
+                        ))
+                        : <div></div>
+                    }
+                </div>
+
+                {isLoggedIn
+                    ? <form onSubmit={(e) => addComment(e)} className="m-2">
+                        <input className="sm:w-auto w-36 border-2 border-green-400 m-2 p-1 rounded-md" type="text" required />
+                        <input className="border-green-400 border-2 font-bold text-xl bg-transparent px-2 py-1 text-green-400 rounded-xl" type="submit" value="Add" />
+                    </form>
+                    : null 
+                }
+
+                <div className="text-red-500 font-bold my-1 sm:my-4">{errorMessage}</div>
             </div>
-
-            {isLoggedIn
-                ? <form onSubmit={(e) => addComment(e)} className="m-2">
-                      <input className="sm:w-auto w-40 border-2 border-green-400 m-2 p-1 rounded-md" type="text" name="" id="" />
-                      <input className="border-green-400 border-2 font-bold text-xl bg-transparent px-2 py-1 text-green-400 rounded-xl" type="submit" value="Add" />
-                  </form>
-                : null 
-            }
-
-            <ValidationError message={errorMessage}/>
         </div>
     )
 }
