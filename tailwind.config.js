2db49bc5d4fbe54bfdef43e69c5bb6ff23ebee73
SalarC123 <salarwcheema@gmail.com>
2021-07-03 12:29:47 -0500

    update purge to fix hamburger menu bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./src/**/*.{js,jsx,ts,tsx}',
+          './src/components/**/*.jsx',
+          './src/components/**/*.js',
+          './public/index.html',
+
+  ],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
